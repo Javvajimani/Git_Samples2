4a4adcc1692ce8950d79207a83064cc5077550b3
Javvajimani <manijavvaji18@gmail.com>
2024-02-20 08:12:41 +0530

    Add project files.

---
+++ Git_Samples2/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void Add(int a = 0, int b = 0)
+{
+	cout << "sum is:" << a + b << endl;
+}
+void Sub(int a = 0, int b = 0, int c = 0)
+{
+	cout << "Diff is :" << a - b - c << endl;
+}
+
+int main()
+{
+	Add(2, 5);
+	Add(2, 8);
+	Add(0, 7);
+	Sub(0, 6, 9);
+	Sub(7, 98, 543);
+
+}
